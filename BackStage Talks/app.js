ae52bb757648dc4a0f8409ce468d383af8248bba
David Thompson <davidbthomp@gmail.com>
2023-05-02 17:03:40 -0500

    Works with keypad but not wheel

--- BackStage Talks/app.js
+++ BackStage Talks/app.js
@@ -4,11 +4,16 @@ const callback = (entries, observer) => {
     entries.forEach(entry => {
         // If the observed element is intersecting the viewport
         if (entry.isIntersecting) {
+            // Get the current active section and remove the active class
+            const currentActive = document.querySelector('.active');
+            if (currentActive) {
+                currentActive.classList.remove('active');
+            }
+            // Add the active class to the observed element
+            entry.target.classList.add('active');
+
             // Set the background color of the document body to the value of the observed element's data-color attribute
             document.body.style.backgroundColor = entry.target.dataset.color;
-
-            // The current data Anchor in view
-            // console.log(entry.target.dataset.anchor);
         }
     });
 };
@@ -18,7 +23,7 @@ const changes = document.querySelectorAll('section');
 
 // Create a new Intersection Observer instance with the defined callback function
 const observer = new IntersectionObserver(callback, {
-    threshold: .5
+    threshold: 0.5
 });
 
 // Observe each section element using the Intersection Observer instance
@@ -26,27 +31,37 @@ changes.forEach(change => {
     observer.observe(change);
 });
 
-
-// 
-//  Scroll Direction
-// 
-
+// Scroll Direction
 
 // Initial state
 let scrollPos = 0;
 let blockScrollEvent = false;
-let timeout;
 
 function detectDirection() {
     // detects new state and compares it with the new one
-    if ((document.body.getBoundingClientRect()).top > scrollPos) {
+    if (document.body.getBoundingClientRect().top > scrollPos) {
         console.log('up');
+
+        const prevSection = document.querySelector('.active').previousElementSibling;
+        if (prevSection) {
+            prevSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
     } else {
         console.log('down');
+
+        const nextSection = document.querySelector('.active').nextElementSibling;
+        if (nextSection) {
+            nextSection.scrollIntoView({
+                behavior: 'smooth'
+            })
+        }
     }
+
     // saves the new position for iteration.
     setTimeout(() => {
-        scrollPos = (document.body.getBoundingClientRect()).top;
+        scrollPos = document.body.getBoundingClientRect().top;
     }, 500);
 }
 
