d676cb88e92d6998e6f19228153d08b9ab9a634e
David Thompson <davidbthomp@gmail.com>
2023-05-03 15:58:54 -0500

    Scrolling on wheel works!

--- BackStage Talks/app.js
+++ BackStage Talks/app.js
@@ -6,11 +6,14 @@ const callback = (entries, observer) => {
         if (entry.isIntersecting) {
             // Get the current active section and remove the active class
             const currentActive = document.querySelector('.active');
+
             if (currentActive) {
                 currentActive.classList.remove('active');
             }
+
             // Add the active class to the observed element
             entry.target.classList.add('active');
+
 
             // Set the background color of the document body to the value of the observed element's data-color attribute
             document.body.style.backgroundColor = entry.target.dataset.color;
@@ -31,62 +34,39 @@ changes.forEach(change => {
     observer.observe(change);
 });
 
-
-
-
 // Scroll Direction
 
 // Initial state
-let scrollPos = 0;
-let blockScrollEvent = false;
 
-function detectDirection() {
-    // detects new state and compares it with the new one
-    if (document.body.getBoundingClientRect().top > scrollPos) {
-        console.log('up');
+function detectDirection(e) {
+    // Find the current active section
+    const currentSection = document.querySelector('section.active');
 
-        const prevSection = document.querySelector('.active').previousElementSibling;
-        if (prevSection) {
-            prevSection.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
-    } else {
-        console.log('down');
+    // Find the next and previous sections
+    const nextSection = currentSection.nextElementSibling;
+    const prevSection = currentSection.previousElementSibling;
 
-        const nextSection = document.querySelector('.active').nextElementSibling;
-        if (nextSection) {
+    // Get the direction of the scroll
+    const delta = e.deltaY;
+
+    // Scroll to the next or previous section, depending on the scroll direction
+    if (delta > 0 && nextSection) {
             nextSection.scrollIntoView({
                 behavior: 'smooth'
             })
-        }
+    } else if (delta < 0 && prevSection) {
+        prevSection.scrollIntoView({
+            behavior: 'smooth'
+        })
     }
-
-    // saves the new position for iteration.
-    setTimeout(() => {
-        scrollPos = document.body.getBoundingClientRect().top;
-    }, 500);
 }
 
-// adding scroll event
-window.addEventListener('scroll', () => {
-    if (blockScrollEvent) {
-        return; // Exit early if scroll event is blocked
-    } else {
-        detectDirection();
-        blockScrollEvent = true;
-        setTimeout(() => {
-            blockScrollEvent = false;
-        }, 500); // unblocks Scroll event after unit MS
-    }
+
+
+// Listen for mouse wheel events
+window.addEventListener('wheel', e => {
+    e.preventDefault();
+    detectDirection(e);
+}, {
+    passive: false
 });
-
-
-    // This will be used for preventing scrolling on mobile sized viewports
-    // if (document.body.clientWidth >= 1024) {
-    //     console.log('Run this')
-    // }
-
-    // Need to make it so scroll direction is decided by Delta
-    // Scroll on mousewheel, works with arrow keys
-    
