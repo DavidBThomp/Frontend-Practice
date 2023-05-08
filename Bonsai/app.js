3588f00e20f3764874cfd7780b585a2268977439
David Thompson <davidbthomp@gmail.com>
2023-05-08 14:02:47 -0500

    item dropdown

--- Bonsai/app.js
+++ Bonsai/app.js
@@ -4,3 +4,8 @@ function navToggle() {
     const navBarLinks = document.getElementsByClassName('navBarLinks')[0];
     navBarLinks.classList.toggle('active')
 }
+
+// Dropdown
+// function dropDown () {
+
+// }
