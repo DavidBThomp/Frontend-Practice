015800a30bb69690c12500a6fd56996a9abbf9cb
David Thompson <davidbthomp@gmail.com>
2023-05-09 16:48:24 -0500

    Faq Section Formatted

--- Bonsai/app.js
+++ Bonsai/app.js
@@ -1,11 +1,5 @@
-
 // Toggle NavBar .active class
 function navToggle() {
     const navBarLinks = document.getElementsByClassName('navBarLinks')[0];
     navBarLinks.classList.toggle('active')
-}
-
-// Dropdown
-// function dropDown () {
-
-// }
+}   
